d6ef25e6e93def08bd07474a58e2f5bd68a0c539
Pegah Emami <you@example.compegahem20@gmail.com git config --global user.name Your>
2022-02-07 15:49:37 -0800

    some files updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -30,7 +30,7 @@ const Navbar = () => {
   return (
     <div className="nav-container">
         <div className="logo-container">
-            <Avatar src={icon} size="large"/>
+            <Avatar src={icon} size="medium"/>
             <Typography.Title level={2} className="logo">
                 <Link to="/" >Crypto-Traker</Link>
             </Typography.Title>
